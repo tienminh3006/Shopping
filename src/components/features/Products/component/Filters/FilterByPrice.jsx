a23dfdca1f901f7074a8347ab548d5bf72fae411
tienminh3006 <tienminh3006nb@gmail.com>
2021-09-28 13:57:04 +0700

    Update
    Update

--- src/components/features/Products/component/Filters/FilterByPrice.jsx
+++ src/components/features/Products/component/Filters/FilterByPrice.jsx
@@ -50,7 +50,7 @@ function FilterByPrice(props) {
           label="Giá thấp nhất"
           // variant="filled"
         />
-        {/* <span>-</span> */}
+        <span>-</span>
         <TextField
           name="salePrice_lte"
           // value={values.salePrice_lte}
