1ca4a820fbc8b2bccf30005e085a9ab8fede858a
tienminh3006 <tienminh3006nb@gmail.com>
2021-09-29 23:05:40 +0700

    Update
    Update

--- src/api/axiosCilent.js
+++ src/api/axiosCilent.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://api.ezfrontend.com/",
+  baseURL: "https://api.ezfrontend.com/",
   headers: {
     "Content-Type": "application/json",
   },
